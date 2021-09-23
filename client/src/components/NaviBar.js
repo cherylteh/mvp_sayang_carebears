31b3e61c187f19e6ab79d8ec620df1f710f3c47f
desk-kris <desk.kris@gmail.com>
2021-09-23 18:14:24 +0800

    added cheryl navbar changes

--- client/src/components/NaviBar.js
+++ client/src/components/NaviBar.js
@@ -2,44 +2,35 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 //import AuthContext from "../context/AuthContext";
 
-const NaviBar = ({loggedIn}) => {
-  //const { loggedIn } = useContext(AuthContext);
-  /* const [loggedIn, setLoggedIn] = useState(undefined);
-
-  const getLoggedIn = () => {
-    fetch("/users/loggedIn", {
-      method: "GET",
-      headers: {
-        "x-access-token": localStorage.getItem("token"),
-      },
-    })
-      .then((result) => {
-        //store it locally
-        setLoggedIn(result.data.status);
-        console.log(`check boolean: ${result.data.status} should be true or false`);
-      })
-      .catch((error) => console.log(error));
-  };
-
-  useEffect(() => {
-    getLoggedIn();
-  }, []); */
-
+const NaviBar = ({ loggedIn }) => {
   return (
-    <div>
-      {loggedIn === false && (
-        <>
-          <Link to="/register">Register</Link>
-          <Link to="/login">Login</Link>
-        </>
-      )}
-      {loggedIn === true && (
-        <>
-          <Link to="/dashboard">Dashboard</Link>
-          <Link to="/medsup">Medication & Supplements</Link>
-          <Link to="/contact">Contacts List</Link>
-        </>
-      )}
+    <div class="container">
+      <nav class="navbar fixed-top navbar-light bg-light">
+        {/* Logo */}
+        <a class="navbar-brand" href="/dashboard">
+          <img
+            src="https://i.postimg.cc/PqZHRJpB/Logo-SAYANG-2.png"
+            height="50"
+            alt="Sayang Logo"
+          ></img>
+        </a>
+
+        <div className="nav justify-content-end">
+          {loggedIn === false && (
+            <>
+              <Link className="nav-item nav-link" to="/register">Register</Link>
+              <Link className="nav-item nav-link" to="/login">Login</Link>
+            </>
+          )}
+          {loggedIn === true && (
+            <>
+              <Link className="nav-item nav-link" to="/dashboard">Dashboard</Link>
+              <Link className="nav-item nav-link" to="/medsup">Medication & Supplements</Link>
+              <Link className="nav-item nav-link" to="/contact">Contacts List</Link>
+            </>
+          )}
+        </div>
+      </nav>
     </div>
   );
 };
